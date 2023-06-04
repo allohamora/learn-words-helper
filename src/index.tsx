94e36624cca970dbf94337ca5ce4777675d30a66
allohamora <herman.hrand@empirenational.com>
2023-06-04 16:47:12 +0300

    feat: add service-worker refresh

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app';
+import { registerSW } from 'virtual:pwa-register';
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    console.log('onNeedRefresh');
+
+    updateSW(true);
+  },
+});
 
 createRoot(document.getElementById('app') as HTMLElement).render(
   <StrictMode>
