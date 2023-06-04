983a68ce64b6e6ee9daf571449f5d9d75121cee1
allohamora <herman.hrand@empirenational.com>
2023-06-04 16:01:25 +0300

    feat: add vite-pwa-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,51 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import linaria from '@linaria/vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    linaria()
+    linaria(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'favicon-16x16.png', 'favicon-32x32.png', 'android-chrome-192x192.png', 'android-chrome-512x512.png'],
+      manifest: {
+          "name": "learn words helper",
+          "short_name": "learn words helper",
+          "description": "learn words helper",
+          "orientation": "any",
+          "icons": [
+              {
+                  "src": "/favicon-16x16.png",
+                  "sizes": "16x16",
+                  "type": "image/png"
+              },
+              {
+                  "src": "/favicon-32x32.png",
+                  "sizes": "32x32",
+                  "type": "image/png"
+              },
+              {
+                  "src": "/favicon.ico",
+                  "sizes": "48x48",
+                  "type": "image/x-icon"
+              },
+              {
+                  "src": "/android-chrome-192x192.png",
+                  "sizes": "192x192",
+                  "type": "image/png"
+              },
+              {
+                  "src": "/android-chrome-512x512.png",
+                  "sizes": "512x512",
+                  "type": "image/png"
+              }
+          ],
+          "theme_color": "#ffffff",
+          "background_color": "#ffffff",
+          "display": "standalone"
+      }
+    }),
   ],
 })
